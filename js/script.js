10744e34d6f21a6807eb7420b5349f97c022b761
DaiNQ <nqdai@vascara.com>
2023-05-11 17:51:17 +0700

    commit ui

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 
 // To understand "{default_offset_pct:0.3}":
-$(function() {
-  $("#img1[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.3});
+$(function () {
+  $("#img1[data-orientation!='vertical']").twentytwenty({ default_offset_pct: 0.3 });
 });
 
-$(function() {
-  $("#img2[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.5});
+$(function () {
+  $("#img2[data-orientation!='vertical']").twentytwenty({ default_offset_pct: 0.5 });
 });
 
-$(function() {
-  $("#img3[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.8});
+$(function () {
+  $("#img3[data-orientation!='vertical']").twentytwenty({ default_offset_pct: 0.8 });
 });
 
-$(function() {
-  $("#img4[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.5});
+$(function () {
+  $("#img4[data-orientation!='vertical']").twentytwenty({ default_offset_pct: 0.5 });
 });
 
-$(function() {
-  $("#img5[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.5});
+$(function () {
+  $("#img5[data-orientation!='vertical']").twentytwenty({ default_offset_pct: 0.5 });
 });
 
 
@@ -314,6 +314,4 @@ function parallaxIt(target, movement) {
 $(window).on("resize scroll", function () {
   slideshow_parameters = $(".slideshow-container")[0].getBoundingClientRect();
 });
-
-//nhap
 
